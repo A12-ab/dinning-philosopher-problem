8b5501c0f55406dbf77e6360dcbfd6734119cd1b
abid saharia <abidsaharia456@gmail.com>
2024-09-20 11:03:00 +0600

    tryToEat function added where philosopher try to get two chopstick

--- Table.java
+++ Table.java
@@ -4,16 +4,36 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Table {
     private final int tableId;
     private final Lock tableLock = new ReentrantLock();
-    private final Lock[] chopsticks = new Lock[5]; // 5 chopsticks per table
-    private int philosophersAtTable = 5; // Start with 5 philosophers
+    private final Lock[] chopsticks = new Lock[5]; 
+    private int philosophersAtTable = 5; 
 
     public Table(int tableId) {
         this.tableId = tableId;
-        // Initialize chopstick locks for this table
+        // added lock to chopstick
         for (int i = 0; i < 5; i++) {
             chopsticks[i] = new ReentrantLock();
         }
     }
+
+    public boolean tryToEat(Philosopher philosopher, Lock[] chopsticks) {
+        tableLock.lock();
+        try {
+            int leftChopstick = philosopher.getId();
+            int rightChopstick = (philosopher.getId() + 1) % 5;
+
+            if (chopsticks[leftChopstick].tryLock()) {
+                if (chopsticks[rightChopstick].tryLock()) {
+                    return true; // Philosopher duita chopstick peye gese
+                } else {
+                    chopsticks[leftChopstick].unlock(); // jodi right chopstick na pai left er tao chere dicche
+                }
+            }
+        } finally {
+            tableLock.unlock();
+        }
+        return false; 
+    }
+
 
   
 }
