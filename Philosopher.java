35d18d2650e5673fd18ab68d81bdd2c32df41b44
ChyYasir <chyyasir2000@gmail.com>
2024-09-28 23:27:47 +0600

    resolved long long issue

--- Philosopher.java
+++ Philosopher.java
@@ -1,4 +1,5 @@
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Philosopher extends Thread {
     private final char label;
@@ -20,22 +21,22 @@ public class Philosopher extends Thread {
     @Override
     public void run() {
         try {
-            while (true) {
+            while (!Thread.currentThread().isInterrupted()) {
                 think();
                 
                 if (!atEmptyTable) {
-                    // khaoar try kortese ekhane
+                    // Try to eat at the main table
                     if (table.tryToEat(this, leftFork, rightFork)) {
                         eat();
                         table.doneEating(this, leftFork, rightFork);
                     } else {
-                        // deadlock so faka table e movie korbe
+                        // Deadlock detected, move to the empty table
                         System.out.println("Philosopher " + label + " moving to the empty table.");
                         emptyTable.movePhilosopherToEmptyTable(this);
                         atEmptyTable = true;
                     }
                 } else {
-                    // Philosopher faka table e chole gese
+                    // Philosopher is at the empty table
                     emptyTable.tryToEat(this, leftFork, rightFork);
                 }
             }
@@ -46,12 +47,12 @@ public class Philosopher extends Thread {
 
     private void think() throws InterruptedException {
         System.out.println("Philosopher " + label + " is thinking.");
-        Thread.sleep((long) (Math.random() * 10000)); // Think for 0-10 seconds
+        Thread.sleep(ThreadLocalRandom.current().nextLong(0, 10000)); // Think for 0-10 seconds
     }
 
     private void eat() throws InterruptedException {
         System.out.println("Philosopher " + label + " is eating.");
-        Thread.sleep((long) (Math.random() * 5000)); // Eat for 0-5 seconds
+        Thread.sleep(ThreadLocalRandom.current().nextLong(0, 5000)); // Eat for 0-5 seconds
     }
 
     public char getLabel() {
