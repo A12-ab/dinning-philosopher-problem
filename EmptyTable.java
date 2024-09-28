346499be0f56f06634a5dd07f92dcd3b9571cc65
ChyYasir <chyyasir2000@gmail.com>
2024-09-28 23:15:19 +0600

    empty table updated

--- EmptyTable.java
+++ EmptyTable.java
@@ -1,19 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-
 public class EmptyTable {
-    private final Lock[] chopsticks = new Lock[5];
+    private final List<Philosopher> philosophers = new ArrayList<>();
+    private final Lock[] forks = new Lock[5];
+    private char lastPhilosopherToMove;
+    private boolean deadlocked = false;
 
     public EmptyTable() {
-        
         for (int i = 0; i < 5; i++) {
-            chopsticks[i] = new ReentrantLock();
+            forks[i] = new ReentrantLock();
         }
+    }
+
+    public synchronized void movePhilosopherToEmptyTable(Philosopher philosopher) {
+        philosophers.add(philosopher);
+        lastPhilosopherToMove = philosopher.getLabel();
+        System.out.println("Philosopher " + lastPhilosopherToMove + " moved to the empty table.");
+        if (philosophers.size() == 5) {
+            checkDeadlock();
+        }
+    }
+
+    public boolean tryToEat(Philosopher philosopher, Lock leftFork, Lock rightFork) throws InterruptedException {
+        // Philosopher tries to eat at the empty table
+        return true;
     }
 
     private void checkDeadlock() {
         if (philosophers.size() == 5) {
+            // Simple deadlock detection: if all 5 philosophers are at the empty table
             deadlocked = true;
             System.out.println("The empty table has deadlocked.");
             System.out.println("Last philosopher to move to the empty table: " + lastPhilosopherToMove);
@@ -23,6 +41,4 @@ public class EmptyTable {
     public boolean isDeadlocked() {
         return deadlocked;
     }
-
-   
 }
